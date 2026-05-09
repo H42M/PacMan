ea4f3944702a71c9419fbe4a5e39c1cc0a45da39
Nicolas Gaubil <nicolas.gaubil@learner.42.tech>
2026-05-09 10:01:47 +0200

    feat: Button render

--- render/RenderObj.py
+++ render/RenderObj.py
@@ -5,11 +5,14 @@ from render.Screen import Screen
 
 class RenderOBJ:
 
-    def __init__(self, screen: Screen) -> None:
+    def __init__(self, screen: Screen,
+                 pos: Optional[tuple[int, int]] = None,
+                 size: Optional[tuple[int, int]] = None) -> None:
         self._screen = screen
 
         self._surface: Optional[pygame.Surface] = None
-        self._pos: Optional[tuple[int, int]] = None
+        self._pos: Optional[tuple[int, int]] = pos
+        self._size: Optional[tuple[int, int]] = size
 
     def render(self):
         pass
