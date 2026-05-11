a33292a05ffd015996aa61b1247d626ecd005171
hugo georges <hugo.georges@learner.42.school>
2026-05-11 11:09:48 +0200

    Add minimal pygame window loop

--- pacman/app.py
+++ pacman/app.py
@@ -1,6 +1,35 @@
+"""Main pygame application loop for Pac-Man."""
+
+import pygame
+from pacman.constants import FPS, WINDOW_HEIGHT, WINDOW_TITLE, WINDOW_WIDTH
 
 
 def run(config_path: str) -> int:
     """Run the Pac-Man application."""
     print(f"Starting Pac-Man with config: {config_path}")
+    try:
+        # pygame initialization
+        pygame.init()
+        screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption(WINDOW_TITLE)
+        clock = pygame.time.Clock()
+        is_running: bool = True
+
+        # game loop
+        while is_running:
+            # events management
+            events = pygame.event.get()
+            for event in events:
+                if event.type == pygame.QUIT:
+                    is_running = False
+
+            # frame display
+            screen.fill("black")
+            pygame.display.flip()
+            clock.tick(FPS)
+    except pygame.error as e:
+        print(e)
+        return 1
+    finally:
+        pygame.quit()
     return 0
