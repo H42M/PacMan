35b0c64191e9b54526f15e3515d361e024cc151b
Nicolas Gaubil <nicolas.gaubil@learner.42.tech>
2026-06-10 08:23:22 +0200

    feat: new HUD

--- pacman/render/RenderGameplay.py
+++ pacman/render/RenderGameplay.py
@@ -1,15 +1,18 @@
 import pygame
+from typing import Optional
 
 from pacman.game_state import GameState
 from pacman.render.RenderMaze import RenderMaze
 from pacman.render.Screen import Screen
 from pacman.render.RenderConfig import RenderConfig
+from pacman.render.RenderObj import RenderOBJ
 
 
-class RenderGameplay:
+class RenderGameplay (RenderOBJ):
     """Render the current gameplay state."""
 
     def __init__(self, screen: Screen, game: GameState) -> None:
+        super().__init__(screen)
         self.screen = screen
         self.game = game
         self.maze_renderer = RenderMaze(screen, game.level.maze)
@@ -92,3 +95,83 @@ class RenderGameplay:
             (screen_width - maze_size) // 2,
             (screen_height - maze_size) // 2,
         )
+
+    @property
+    def size(self) -> Optional[tuple[int, int]]:
+        return self._size
+
+    @size.setter
+    def size(self, value: Optional[tuple[int, int]]) -> None:
+        if value is not None:
+            self._size = value
+            self.maze_renderer.size = value
+
+    @property
+    def w(self) -> Optional[int]:
+        if self._size:
+            return self._size[0]
+        return None
+
+    @w.setter
+    def w(self, value: Optional[int]) -> None:
+        if value is not None:
+            if self._size:
+                self._size = (value, self._size[1])
+            else:
+                self._size = (value, 0)
+            self.maze_renderer.size = self._size
+
+    @property
+    def h(self) -> Optional[int]:
+        if self._size is not None:
+            return self._size[1]
+        return None
+
+    @h.setter
+    def h(self, value: Optional[int]) -> None:
+        if value is not None:
+            if self._size:
+                self._size = (self._size[0], value)
+            else:
+                self._size = (0, value)
+            self.maze_renderer.size = self._size
+
+    @property
+    def pos(self) -> Optional[tuple[int, int]]:
+        return self._pos
+
+    @pos.setter
+    def pos(self, value: Optional[tuple[int, int]]) -> None:
+        if value is not None:
+            self._pos = value
+            self.maze_renderer.pos = self._pos
+
+    @property
+    def x(self) -> Optional[int]:
+        if self._pos is not None:
+            return self._pos[0]
+        return None
+
+    @x.setter
+    def x(self, value: Optional[int]) -> None:
+        if value is not None:
+            if self._pos:
+                self._pos = (value, self._pos[1])
+            else:
+                self._pos = (value, 0)
+            self.maze_renderer.pos = self._pos
+
+    @property
+    def y(self) -> Optional[int]:
+        if self._pos is not None:
+            return self._pos[1]
+        return None
+
+    @y.setter
+    def y(self, value: Optional[int]) -> None:
+        if value is not None:
+            if self._pos:
+                self._pos = (self._pos[0], value)
+            else:
+                self._pos = (0, value)
+            self.maze_renderer.pos = self._pos
